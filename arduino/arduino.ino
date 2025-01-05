b8507d8cc870715da157803e1bd883d6dea209c1
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2025-01-05 14:17:02 -0600

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,25 +1,25 @@
 void setup() {
 Serial.begin(9600);
-pinMode(13,OUTPUT);
-pinMode(12,INPUT_PULLUP);
+pinMode(13,OUTPUT);// switching output pin
+pinMode(12,INPUT_PULLUP);// on/off hook collector pin
 }
 bool currentState=false;
 void loop() {
- Serial.println(digitalRead(12)+(2*digitalRead(13)));
-noTone(8);
+ Serial.println(digitalRead(12)+(2*digitalRead(13)));// write current state to serial port
+noTone(8);//disable tone
 if(currentState){
-  digitalWrite(13,HIGH);
-  tone(8,1024);  
+  digitalWrite(13,HIGH);// switch on
+  tone(8,1024);  // switch on tone
 }else{
-  digitalWrite(13,LOW);
-  tone(8,512);
+  digitalWrite(13,LOW);//switch off
+  tone(8,512);  // switch off tone
 }
-delay(2048);
-if(digitalRead(12)==LOW){
-  delay(2048);
-  tone(8,256);
-  delay(2048);
+delay(2048);// wait anout 2 seconds
+if(digitalRead(12)==LOW){ // if phone os active
+  delay(2048);// wait 2 seconds
+  tone(8,256);// update tone
+  delay(2048);//wait another 2 seconds
   if(digitalRead(12)==LOW)
-  currentState=!currentState;
+  currentState=!currentState;//invert current state
 }
 }
